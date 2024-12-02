76b49df6e17cb17e4287f757f79e575e27e5baa1
Aşık Üveys Uzunsakal <asikuveys1@gmail.com>
2024-12-02 16:30:43 +0300

    Genel düzeltme.

--- libs/functions.php
+++ libs/functions.php
@@ -1,35 +1,47 @@
 <?php
 
-    function kursEkle(&$kurslar, string $baslik,string $altBaslik,string $resim,string $yayinTarihi,int $yorumSayisi=0,int $begeniSayisi=0,bool $onay=true){
-        $yeni_kurs[count($kurslar) + 1] = array(
-            "baslik" => $baslik,
-            "altBaslik" => $altBaslik,
-            "resim" => $resim,
-            "yayinTarihi" => $yayinTarihi,
-            "yorumSayisi" => $yorumSayisi,
-            "begeniSayisi" => $begeniSayisi,
-            "onay" => $onay
-        ); 
+function getDb() {
+    $myfile = fopen("db.json","r");
+    $size = filesize("db.json");
+    $data = json_decode(fread($myfile, $size), true);    
+    fclose($myfile);
+    return $data;
+}
 
-        $kurslar = array_merge($kurslar, $yeni_kurs);
-    }
+function kursEkle(string $baslik,string $altBaslik,string $resim,string $yayinTarihi,int $yorumSayisi=0,int $begeniSayisi=0, bool $onay=true) {
+    $db = getDb();
+    
+    array_push($db["kurslar"], array(
+        "baslik" => $baslik,
+        "altBaslik" => $altBaslik,
+        "resim" => $resim,
+        "yayinTarihi" => $yayinTarihi,
+        "yorumSayisi" => $yorumSayisi,
+        "begeniSayisi" => $begeniSayisi,
+        "onay" => $onay
+    ));
 
-    function urlDuzenle($baslik){
-        return str_replace([" ","ç","@","."],["-","c","","-"],strtolower($baslik));
-    }
+    $myfile  = fopen("db.json","w");
+    fwrite($myfile, json_encode($db, JSON_PRETTY_PRINT));
+    fclose($myfile);
+}
 
-    function kisaAciklama($altBaslik){
-        if (strlen($altBaslik) > 50 ){
-            return substr($altBaslik,0,50)."...";
-        }else{
-            return $altBaslik;
-        }  
-    }
+function urlDuzenle($baslik) {
+    return str_replace([" ","ç","@","."],["-","c","","-"],strtolower($baslik));
+}
 
-    function safe_html($data){
-        $data = trim($data);
-        $data = stripcslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
+function kisaAciklama($altBaslik) {
+    if (strlen($altBaslik) > 50) {
+        return substr($altBaslik,0,50)."..."; 
+    } else {
+        return $altBaslik;
     }
+}
+
+function safe_html($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data ;
+}
 ?>
